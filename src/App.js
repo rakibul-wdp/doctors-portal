b4e23344bccc5ba368d366d833455d8affc89915
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-12 01:22:06 +0600

    customize banner and add background image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className='px-12'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className='navbar bg-base-100'>
+    <div className='navbar bg-base-100 justify-between'>
       <div className='navbar-start'>
         <div className='dropdown'>
           <label tabindex='0' className='btn btn-ghost lg:hidden'>
