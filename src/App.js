2d6c8f5fa129b78e6b89368ac887abebb6fe230a
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-12 00:14:38 +0600

    add navbar and two component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
+
 function App() {
   return (
     <div>
-      <h2>Hello World...!!!</h2>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
   );
 }
