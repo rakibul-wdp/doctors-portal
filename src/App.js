cfb90c24006148304dec4d7a6877f52835f76655
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-12 00:43:27 +0600

    change some navbar code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
+        <Route path='about' element={<About />} />
+        <Route path='login' element={<Login />} />
       </Routes>
     </div>
   );
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -2,6 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const menuItems = (
+    <>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to={'/appointment'}>Appointment</Link>
+      </li>
+
+      <li>
+        <Link to={'/review'}>Review</Link>
+      </li>
+      <li>
+        <Link to={'/contact'}>Contact</Link>
+      </li>
+      <li>
+        <Link to='/about'>About</Link>
+      </li>
+      <li>
+        <Link to={'/login'}>Login</Link>
+      </li>
+    </>
+  );
   return (
     <div className='navbar bg-base-100'>
       <div className='navbar-start'>
@@ -18,49 +41,15 @@ const Navbar = () => {
             </svg>
           </label>
           <ul tabindex='0' className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link>Appointment</Link>
-            </li>
-            <li>
-              <Link>Reviews</Link>
-            </li>
-            <li>
-              <Link>Contact Us</Link>
-            </li>
-            <li>
-              <Link>Login</Link>
-            </li>
+            {menuItems}
           </ul>
         </div>
-        <Link className='btn btn-ghost normal-case text-xl'>Doctors Portal</Link>
+        <Link to={'/'} className='btn btn-ghost normal-case text-xl'>
+          Doctors Portal
+        </Link>
       </div>
       <div className='navbar-center hidden lg:flex'>
-        <ul className='menu menu-horizontal p-0'>
-          <li>
-            <Link>Home</Link>
-          </li>
-          <li>
-            <Link>About</Link>
-          </li>
-          <li>
-            <Link>Appointment</Link>
-          </li>
-          <li>
-            <Link>Reviews</Link>
-          </li>
-          <li>
-            <Link>Contact Us</Link>
-          </li>
-          <li>
-            <Link>Login</Link>
-          </li>
-        </ul>
+        <ul className='menu menu-horizontal p-0'>{menuItems}</ul>
       </div>
     </div>
   );
