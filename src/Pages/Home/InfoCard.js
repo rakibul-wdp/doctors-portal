9067366dd4e7149dc3a1c611a09f95ee17dc0d7e
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-12 12:20:09 +0600

    add information section

---
+++ src/Pages/Home/InfoCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const InfoCard = ({ img }) => {
+  return (
+    <div className='card lg:card-side bg-gradient-to-r from-secondary to-primary shadow-xl'>
+      <figure>
+        <img src={img} alt='Album' />
+      </figure>
+      <div className='card-body'>
+        <h2 className='card-title'>New album is released!</h2>
+        <p>Click the button to listen on Spotiwhy app.</p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
