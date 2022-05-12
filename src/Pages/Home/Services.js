8fe90405dcfa7b00b8883589986f3ff64b691bf4
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-13 02:18:06 +0600

    add services section with video

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -5,16 +5,36 @@ import cavity from '../../assets/images/cavity.png';
 import whitening from '../../assets/images/whitening.png';
 
 const Services = () => {
+  const services = [
+    {
+      _id: 1,
+      name: 'Fluoride Treatment',
+      description: 'If a dog chews shoes whose shoes does he choose?',
+      img: fluoride,
+    },
+    {
+      _id: 2,
+      name: 'Cavity Filling',
+      description: 'If a dog chews shoes whose shoes does he choose?',
+      img: cavity,
+    },
+    {
+      _id: 3,
+      name: 'Teeth Whitening',
+      description: 'If a dog chews shoes whose shoes does he choose?',
+      img: whitening,
+    },
+  ];
   return (
-    <section className='my-20'>
+    <section className='my-28'>
       <div className='text-center mb-10'>
-        <h3 className='uppercase font-bold text-secondary text-lg'>Our Services</h3>
+        <h3 className='uppercase font-bold text-secondary text-xl'>Our Services</h3>
         <p className='text-4xl'>Services We Provide</p>
       </div>
-      <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-        <Service img={fluoride} cardTitle={'Fluoride Treatment'} />
-        <Service img={cavity} cardTitle={'Cavity Filling'} />
-        <Service img={whitening} cardTitle={'Teeth Whitening'} />
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+        {services.map((service) => (
+          <Service key={service._id} service={service} />
+        ))}
       </div>
     </section>
   );
