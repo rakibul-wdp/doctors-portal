1edcfa72d20d254da89c3d1fd45df695972515e2
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-13 01:00:00 +0600

    add info section with dynamic background color

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,9 +7,9 @@ import phone from '../../assets/icons/phone.svg';
 const Info = () => {
   return (
     <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-      <InfoCard img={clock} />
-      <InfoCard img={marker} />
-      <InfoCard img={phone} />
+      <InfoCard cardTitle='Opening Hours' bgClass='bg-gradient-to-r from-secondary to-primary' img={clock} />
+      <InfoCard cardTitle='Our Locations' bgClass='bg-accent' img={marker} />
+      <InfoCard cardTitle='Contact Us' bgClass='bg-gradient-to-r from-secondary to-primary' img={phone} />
     </div>
   );
 };
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ img, cardTitle, bgClass }) => {
   return (
-    <div className='card lg:card-side bg-gradient-to-r from-secondary to-primary shadow-xl'>
-      <figure>
+    <div className={`card lg:card-side shadow-xl ${bgClass}`}>
+      <figure className='pl-5'>
         <img src={img} alt='Album' />
       </figure>
-      <div className='card-body'>
-        <h2 className='card-title'>New album is released!</h2>
+      <div className='card-body text-white'>
+        <h2 className='card-title'>{cardTitle}</h2>
         <p>Click the button to listen on Spotiwhy app.</p>
       </div>
     </div>
