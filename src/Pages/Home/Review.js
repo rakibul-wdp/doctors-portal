6d5392e69452fa60dae471fe3647091e5507838d
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-13 11:48:31 +0600

    edit some change with video

---
+++ src/Pages/Home/Review.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Review = ({ review }) => {
+  return (
+    <div className='card lg:max-w-lg bg-base-100 shadow-xl'>
+      <div className='card-body'>
+        <p className='mb-5'>{review.review}</p>
+        <div className='flex items-center'>
+          <div className='avatar'>
+            <div className='w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2'>
+              <img src={review.img} alt='' />
+            </div>
+          </div>
+          <div className='ml-5'>
+            <h4 className='text-lg font-bold'>{review.name}</h4>
+            <h5>{review.location}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
--- src/Pages/Home/Testimonial.js
+++ src/Pages/Home/Testimonial.js
@@ -3,52 +3,52 @@ import quote from '../../assets/icons/quote.svg';
 import people1 from '../../assets/images/people1.png';
 import people2 from '../../assets/images/people2.png';
 import people3 from '../../assets/images/people3.png';
-import PatientReview from './PatientReview';
+import Review from './Review';
 
 const Testimonial = () => {
-  const patientsSays = [
+  const reviews = [
     {
       _id: 1,
       name: 'Arnob Boyati',
       img: people1,
-      place: 'Kalaivita',
-      description:
+      location: 'Kalaivita',
+      review:
         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, in temporibus quia facere officiis tenetur? Sunt ab laudantium suscipit repellat',
     },
     {
       _id: 2,
       name: 'Omok Tomok',
       img: people2,
-      place: 'Uchrong',
-      description:
+      location: 'Uchrong',
+      review:
         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, in temporibus quia facere officiis tenetur? Sunt ab laudantium suscipit repellat',
     },
     {
       _id: 3,
       name: 'Borobila',
       img: people3,
-      place: 'Kalaivita',
-      description:
+      location: 'Kalaivita',
+      review:
         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, in temporibus quia facere officiis tenetur? Sunt ab laudantium suscipit repellat',
     },
   ];
   return (
-    <div>
+    <section>
       <div className='flex justify-between'>
         <div>
           <h3 className='text-xl font-bold text-secondary'>Testimonial</h3>
           <h2 className='text-4xl'>What Our Patients Says</h2>
         </div>
         <div>
-          <img src={quote} alt='' />
+          <img className='w-24 lg:w-48' src={quote} alt='' />
         </div>
       </div>
       <div className='grid sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-10'>
-        {patientsSays.map((patient) => (
-          <PatientReview key={patient._id} patient={patient} />
+        {reviews.map((review) => (
+          <Review key={review._id} review={review} />
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
