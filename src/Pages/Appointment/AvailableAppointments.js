51920e240d059fb5d4b3de152e9f25989d92acf7
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-16 12:39:17 +0600

    add book appointment card and work some conditional statement

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,12 +1,25 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service';
 
 const AvailableAppointments = ({ date }) => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch('services.json')
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
-    <div>
+    <div className='mb-24'>
       <h3 className='text-center font-bold text-xl my-20 text-secondary'>
         Available Appointments on {format(date, 'PP')}
       </h3>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {services.map((service) => (
+          <Service key={service._id} service={service} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Appointment/Service.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Service = ({ service }) => {
+  const { name, slots } = service;
+  return (
+    <div className='card lg:max-w-lg bg-base-100 shadow-xl text-center'>
+      <div className='card-body'>
+        <h2 className='text-center text-secondary font-bold text-xl'>{name}</h2>
+        <p>{slots.length > 0 ? <span>{slots[0]}</span> : <span className='text-red-500'>Try another date.</span>}</p>
+        <p>
+          {slots.length} {slots.length > 1 ? 'spaces' : 'space'} available
+        </p>
+        <div className='card-actions justify-center'>
+          <button
+            disabled={slots.length === 0}
+            className='btn btn-primary text-white bg-gradient-to-r from-secondary to-primary'
+          >
+            Book Appointment
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
