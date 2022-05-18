16bd0ccd82f8b52c05ac981c1f667d45234b5825
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-18 15:35:50 +0600

    add login page and also add form from react hook form etc.

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,21 +1,103 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { useForm } from 'react-hook-form';
+import Loading from '../Shared/Loading';
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const [signInWithEmailAndPassword, eUser, eLoading, eError] = useSignInWithEmailAndPassword(auth);
 
-  if (gUser) {
-    console.log(gUser);
+  let signInError;
+  if (gError || eError) {
+    signInError = <p className='mb-3 text-red-500'>{gError?.message || eError?.message}</p>;
   }
+  if (gLoading || eLoading) {
+    return <Loading />;
+  }
+  if (gUser || eUser) {
+    console.log(gUser, eUser);
+  }
+  const onSubmit = (data) => {
+    console.log(data);
+    signInWithEmailAndPassword(data.email, data.password);
+  };
   return (
     <div className='flex justify-center items-center h-[80vh]'>
       <div className='card w-96 bg-base-100 shadow-xl'>
         <div className='card-body'>
           <h2 className='text-center text-2xl font-bold'>Login</h2>
-
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='form-control w-full max-w-xs'>
+              <label className='label'>
+                <span className='label-text'>Email</span>
+              </label>
+              <input
+                type='email'
+                placeholder='Your Email'
+                className='input input-bordered w-full max-w-xs text-lg'
+                {...register('email', {
+                  required: {
+                    value: true,
+                    message: 'Email is Required',
+                  },
+                  pattern: {
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: 'Provide a valid Email',
+                  },
+                })}
+              />
+              <label className='label'>
+                {errors.email?.type === 'required' && (
+                  <span className='label-text-alt text-red-500'>{errors.email.message}</span>
+                )}
+                {errors.email?.type === 'pattern' && (
+                  <span className='label-text-alt text-red-500'>{errors.email.message}</span>
+                )}
+              </label>
+            </div>
+            <div className='form-control w-full max-w-xs'>
+              <label className='label'>
+                <span className='label-text'>Password</span>
+              </label>
+              <input
+                type='password'
+                placeholder='Password'
+                className='input input-bordered w-full max-w-xs text-lg'
+                {...register('password', {
+                  required: {
+                    value: true,
+                    message: 'Password is Required',
+                  },
+                  minLength: {
+                    value: 6,
+                    message: 'Must be 6 characters or longer',
+                  },
+                })}
+              />
+              <label className='label'>
+                {errors.password?.type === 'required' && (
+                  <span className='label-text-alt text-red-500'>{errors.password.message}</span>
+                )}
+                {errors.password?.type === 'minLength' && (
+                  <span className='label-text-alt text-red-500'>{errors.password.message}</span>
+                )}
+              </label>
+            </div>
+            {signInError}
+            <input
+              className='w-full max-w-xs btn text-lg uppercase text-white font-bold bg-accent'
+              type='submit'
+              value='Login'
+            />
+          </form>
           <div className='divider'>OR</div>
-          <button onClick={() => signInWithGoogle()} className='btn btn-outline'>
+          <button onClick={() => signInWithGoogle()} className='btn btn-outline text-lg'>
             Continue with Google
           </button>
         </div>
