b47a549ec6c82c370cd2cc19f39a0f782253b95a
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-20 05:46:27 +0600

    fetch data for user make to admin

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import UserRow from './UserRow';
 
 const Users = () => {
-  const { data: users, isLoading } = useQuery('users', () =>
-    fetch('http://localhost:5000/user').then((res) => res.json())
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery('users', () =>
+    fetch('http://localhost:5000/user', {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    }).then((res) => res.json())
   );
 
   if (isLoading) {
@@ -13,6 +23,23 @@ const Users = () => {
   return (
     <div>
       <h2 className='mb-1 text-xl font-bold'>All Users: {users.length}</h2>
+      <div className='overflow-x-auto'>
+        <table className='table w-full'>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <UserRow key={user._id} user={user} refetch={refetch} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
