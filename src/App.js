8f9e5fac155ab7e0e28d9ae8572a1f94e0df48d5
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-20 06:35:41 +0600

    create requireAdmin and use it app.js for all users requireAuth

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import MyAppointments from './Pages/Dashboard/MyAppointments';
 import MyReview from './Pages/Dashboard/MyReview';
 import MyHistory from './Pages/Dashboard/MyHistory';
 import Users from './Pages/Dashboard/Users';
+import RequireAdmin from './Pages/Login/RequireAdmin';
 
 function App() {
   return (
@@ -40,7 +41,14 @@ function App() {
           <Route index element={<MyAppointments />} />
           <Route path='review' element={<MyReview />} />
           <Route path='history' element={<MyHistory />} />
-          <Route path='users' element={<Users />} />
+          <Route
+            path='users'
+            element={
+              <RequireAdmin>
+                <Users />
+              </RequireAdmin>
+            }
+          />
         </Route>
         <Route path='login' element={<Login />} />
         <Route path='signup' element={<SignUp />} />
