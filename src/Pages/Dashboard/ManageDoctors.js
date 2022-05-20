672c702c03d2a7f8cbd78c66d97a3cc7b708ad3b
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-20 20:13:06 +0600

    fetch doctor data for delete doctor

--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
-  const { data: doctors, isLoading } = useQuery('doctors', () =>
+  const {
+    data: doctors,
+    isLoading,
+    refetch,
+  } = useQuery('doctors', () =>
     fetch('http://localhost:5000/doctor', {
       headers: {
         authorization: `Bearer ${localStorage.getItem('accessToken')}`,
@@ -17,6 +22,24 @@ const ManageDoctors = () => {
   return (
     <div>
       <h2 className='text-xl mb-1 font-bold'>Manage Doctors: {doctors.length}</h2>
+      <div className='overflow-x-auto'>
+        <table className='table w-full'>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Specialty</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {doctors.map((doctor, index) => (
+              <DoctorRow key={doctor._id} doctor={doctor} index={index} refetch={refetch} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
